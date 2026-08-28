02d2f88f2bde71fbeb7c42b4a32957b1938d3c93
Zohltar <mathieu.plourde@gmail.com>
2026-08-28 09:19:25 -0400

    Cleanup: remove retired multiplayer HD facade

--- src/multiplayer-hd-vehicles.js
+++
@@ -1,7 +0,0 @@
-// Compatibility entrypoint retained for older imports.
-// M3 owns the only remote HD registry/cache implementation.
-export {
-  createRemoteHdVehicle,
-  supportsRemoteHdVehicle,
-  remoteHdDiagnostics
-} from './multiplayer-hd-vehicles-m3.js';
